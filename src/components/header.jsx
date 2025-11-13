8133b7bc74f2b55b6fdb5e6e9f2353b24d653af8
tejas-git05 <tejasshewale0502@gmail.com>
2025-11-13 22:32:19 +0530

    Setup Supabase and Clerk authentication, and create initial landing page with Shadcn UI

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 const Header = () => {
   return (
     <>
-        <nav className='py-4 flex justify-between items-center '> 
+        <nav className=' flex justify-between items-center '> 
             <Link>
                 <img src="/JobPortalLogo.png" alt="Logo" className='h-30' />
             </Link>
 
             <Button variant="default">Login</Button>
+
+
+      {/* <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn> */}
+
+
         </nav>
     </>
   )
